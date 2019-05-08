8a1f2e5fd115baa668e102dc9992f7dcb6bd546e
Jesús Díaz <Jesusr_Diaz@bose.com>
2019-05-08 09:17:23 -0300

    Add model validations

--- models/user.go
+++ models/user.go
@@ -2,7 +2,7 @@ package models
 
 // User is the main model
 type User struct {
-	FirstName string `gorm:"not null" json:"first_name"`
-	LastName  string `gorm:"not null" json:"last_name"`
-	Email     string `gorm:"not null" json:"email"`
+	FirstName string `gorm:"not null" binding:"required" json:"first_name"`
+	LastName  string `gorm:"not null" binding:"required" json:"last_name"`
+	Email     string `gorm:"not null" binding:"required,email" json:"email"`
 }
